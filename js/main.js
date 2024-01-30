f316ba6ef54507819b3f691d32c82cb1fd0ebd8f
opezdola <nekras.2001@mail.ru>
2024-01-30 18:32:53 +0700

    условный рендеринг

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ var app = new Vue({
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        linkText: "More products like this"
+        linkText: "More products like this",
+        inStock: true,
+        inventory: 100,
+        onSale: true
+
     }
 })
