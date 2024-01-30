8bcc1472848cca57f5bf80dbf7ca96fef62ce7f2
opezdola <nekras.2001@mail.ru>
2024-01-30 18:40:48 +0700

    Рендеринг списков

--- js/main.js
+++ js/main.js
@@ -8,8 +8,22 @@ var app = new Vue({
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         linkText: "More products like this",
         inStock: true,
-        inventory: 100,
-        onSale: true
+        inventory: 1,
+        onSale: false,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+
+
 
     }
 })
